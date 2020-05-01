7399e75de2db4e3a6efb83a22160688b4c997e4f
julpark-rh <yonhyun@gmail.com>
2020-04-30 19:15:54 -0700

    Adding rbd feature in right place
    Signed-off-by: julpark-rh <yonhyun@gmail.com>

--- qa/workunits/rbd/krbd_latest_osdmap_on_map.sh
+++ qa/workunits/rbd/krbd_latest_osdmap_on_map.sh
@@ -3,7 +3,7 @@
 set -ex
 
 function run_test() {
-    ceph osd pool create foo 12 --image-feature layering
+    ceph osd pool create foo 12
     rbd pool init foo
     rbd create --size 1 foo/img
 
@@ -20,7 +20,7 @@ for ((i = 0; i < $NUM_ITER; i++)); do
     run_test
 done
 
-rbd create --size 1 img
+rbd create --size 1 img --image-feature layering
 DEV=$(sudo rbd map img)
 for ((i = 0; i < $NUM_ITER; i++)); do
     run_test
